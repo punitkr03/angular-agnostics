28f717edce97892ff8c94ae3642d48414503dbc4
punitkr03 <tagpunitkumar@gmail.com>
2024-05-27 19:00:51 +0530

    todo app in 1 hr?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+
+interface Todo {
+  id: number;
+  todoText: string;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-agnostics';
+  todos: Todo[] = [];
+  todoText: string = '';
+
+  addTodo(todoText: string) {
+    if (!todoText) {
+      alert('Cannot add empty todo');
+      return;
+    }
+    this.todos.push({
+      id: this.todos.length + 1,
+      todoText,
+    });
+    this.todoText = '';
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
 }
